99e631e78428bd3cbf808806efba397cda83b504
Hugh Morrison <hmorri32@gmail.com>
2017-10-27 10:53:33 -0600

    adds yung rating to reviewz

--- spec/features/user_sees_booke_info_show_page_spec.rb
+++ spec/features/user_sees_booke_info_show_page_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 RSpec.feature "when a user visits the book show page" do
   let!(:book) { Book.create(title: 'cool')}
   let!(:user) { User.create(name: 'big book guy') }
-  let!(:review) { book.reviews.create(body: 'super chill book', user: user)}
+  let!(:review) { book.reviews.create(body: 'super chill book', rating: 1, user: user)}
   before { visit book_path(book) }
 
   scenario "I see the book title and a list of reviews" do
-    # review = book.reviews.create(body: 'super chill book', user: user)
-
     expect(page).to have_content(book.title)
     expect(page).to have_content("ALL THESE REVIEWS")
     expect(page).to have_content(user.name)
     expect(page).to have_content(review.body)
+  end
+
+  scenario "I see the book's rating" do
+    # expect(page).to have_content(review.rating)
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
   context "validations" do
     it { should validate_presence_of(:body) }
+    it { should validate_presence_of(:rating) }
   end
 
   context "relationships" do

62647757f12616496707e0e30d5ff1fd14e4cb66
Adrian Lara <adrianblara@gmail.com>
2017-10-27 10:30:08 -0600

    test showing book show has body and rating written and passed as well

--- spec/features/guest_can_visit_book_show_page_spec.rb
+++ spec/features/guest_can_visit_book_show_page_spec.rb
@@ -1,15 +1,22 @@
 require 'rails_helper'
 
 describe "When I visit a book show page" do
+
+  before(:each) do
+    @book = Book.create(title: "Book 1")
+    @user = User.create(name: "User 1")
+    @review = Review.create(book: @book, user: @user, rating: 5, body: "This was a great book!")
+
+    visit book_path(@book)
+  end
+
   it "I see the book title and a list of reviews for that book, including the body and the user that created that review." do
-    book = Book.create(title: "Book 1")
-    user = User.create(name: "User 1")
-    review = Review.create(book: book, user: user, rating: 5, body: "This was a great book!")
+    expect(page).to have_content(@book.title)
+    expect(page).to have_content(@user.name)
+    expect(page).to have_content(@review.body)
+  end
 
-    visit book_path(book)
-
-    expect(page).to have_content(book.title)
-    expect(page).to have_content(user.name)
+  it "I also see the rating that the user gave the book." do
+    expect(page).to have_content(@review.rating)
   end
 end
-#  I also see the rating that the user gave the book."

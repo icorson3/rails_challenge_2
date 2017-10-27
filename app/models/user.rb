eb4500f505881d7eb4fa41d6634c8eb2b1b45575
Hugh Morrison <hmorri32@gmail.com>
2017-10-27 10:24:05 -0600

    validate user name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-  has_many :reviews
+  validates :name, presence: true
+  has_many  :reviews
 end
--- spec/features/user_sees_booke_info_show_page_spec.rb
+++ spec/features/user_sees_booke_info_show_page_spec.rb
@@ -1,10 +1,14 @@
 require 'rails_helper'
 
 RSpec.feature "when a user visits the book show page" do
-  let!(:book) {Book.create(title: 'cool')}
+  let!(:book) { Book.create(title: 'cool')}
+  let!(:user) { User.create }
 
-  before { visit book_path(book)}
+  before { visit book_path(book) }
+
   scenario "i see the book title and a list of reviews" do
     expect(page).to have_content("#{book.title}")
+
+
   end
 end

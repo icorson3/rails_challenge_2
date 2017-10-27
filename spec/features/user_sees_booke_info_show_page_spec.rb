7c18a2f31a7d2ff4c42fc0ff230303a065879461
Hugh Morrison <hmorri32@gmail.com>
2017-10-27 10:50:55 -0600

    waow. it 1 kinda.

--- spec/features/user_sees_booke_info_show_page_spec.rb
+++ spec/features/user_sees_booke_info_show_page_spec.rb
@@ -3,12 +3,15 @@ require 'rails_helper'
 RSpec.feature "when a user visits the book show page" do
   let!(:book) { Book.create(title: 'cool')}
   let!(:user) { User.create(name: 'big book guy') }
-
+  let!(:review) { book.reviews.create(body: 'super chill book', user: user)}
   before { visit book_path(book) }
 
-  scenario "i see the book title and a list of reviews" do
-    review = user.reviews.create(body: 'super chill book', book: book)
-    byebug
-    expect(page).to have_content("#{book.title}")
+  scenario "I see the book title and a list of reviews" do
+    # review = book.reviews.create(body: 'super chill book', user: user)
+
+    expect(page).to have_content(book.title)
+    expect(page).to have_content("ALL THESE REVIEWS")
+    expect(page).to have_content(user.name)
+    expect(page).to have_content(review.body)
   end
 end

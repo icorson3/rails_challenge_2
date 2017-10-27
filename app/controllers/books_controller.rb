36cd2a9333d2861cd471006799bfa085776abe7f
Hugh Morrison <hmorri32@gmail.com>
2017-10-27 10:59:00 -0600

    rating books, avg, min, max, etc etcet c

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,5 +2,8 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @average_rating = @book.reviews.average(:rating)
+    @max = @book.reviews.maximum(:rating)
+    @min = @book.reviews.minimum(:rating)
   end
 end
--- spec/features/user_sees_booke_info_show_page_spec.rb
+++ spec/features/user_sees_booke_info_show_page_spec.rb
@@ -4,6 +4,9 @@ RSpec.feature "when a user visits the book show page" do
   let!(:book) { Book.create(title: 'cool')}
   let!(:user) { User.create(name: 'big book guy') }
   let!(:review) { book.reviews.create(body: 'super chill book', rating: 1, user: user)}
+  let!(:review_2) { book.reviews.create(body: 'kinda chill', rating: 3, user: user)}
+  let!(:review_3) { book.reviews.create(body: 'barbaric', rating: 1, user: user)}
+  let!(:review_4) { book.reviews.create(body: 'post post noir', rating: 4, user: user)}
   before { visit book_path(book) }
 
   scenario "I see the book title and a list of reviews" do
@@ -14,6 +17,18 @@ RSpec.feature "when a user visits the book show page" do
   end
 
   scenario "I see the book's rating" do
-    # expect(page).to have_content(review.rating)
+    expect(page).to have_content(review.rating)
+  end
+
+  scenario "I see a books average rating" do
+    expect(page).to have_content('Average Rating: 2.25')
+  end
+
+  scenario "I see a books maximum rating" do
+    expect(page).to have_content('Maximum Rating: 4')
+  end
+
+  scenario "I see a books minimum rating" do
+    expect(page).to have_content('Minimum Rating: 1')
   end
 end

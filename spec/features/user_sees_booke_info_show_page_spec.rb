8d3d21fd9674dd0aa4dcd24ee2fcdfa9fe133578
Hugh Morrison <hmorri32@gmail.com>
2017-10-27 10:16:34 -0600

    model test book

---
+++ spec/features/user_sees_booke_info_show_page_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.feature "when a user visits the book show page" do
+  let!(:book) {Book.create(title: 'cool')}
+
+  before { visit book_path(book)}
+  scenario "i see the book title and a list of reviews" do
+    expect(page).to have_content("#{book.title}")
+  end
+end

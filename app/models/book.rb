ac613a3be45a836761a0c5e0e5700653ed916d4c
Anna Lewis <anna.noel.lewis@gmail.com>
2017-10-27 10:59:39 -0600

    make feature tests pass, still needs model tests and refactoring

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,23 @@
 class Book < ApplicationRecord
   has_many :reviews
+
+  def sum_reviews
+    reviews.sum(:rating)
+  end
+
+  def count_reviews
+    reviews.count
+  end
+
+  def average_rating
+    sum_reviews/count_reviews
+  end
+
+  def highest_rating
+    reviews.max_by{ |review| review.rating }
+  end
+
+  def lowest_rating
+    reviews.min_by{ |review| review.rating }
+  end
 end
--- spec/features/user_can_see_a_book_spec.rb
+++ spec/features/user_can_see_a_book_spec.rb
@@ -1,29 +1,12 @@
-# list of reviews for that book, including the body and the user that created that review. I also see the rating (1-5; you do not need to validate that this falls within this range) that the user gave the book.
-#
-# When I visit the book show page, I see the average rating for that book.
-#
-# When I visit the book show page, I see a header that includes the highest rating for that book (e.g "Highest Rating: 4").
-#
-# When I visit the book show page, I see a header that includes the lowest rating for that book (e.g "Lowest Rating: 1").
-#
-# When I visit the book show page, under the header for highest rating, I see the body of that review and the name of the reviewer that submitted that review.
-#
-# When I visit the book show page, under the header for lowest rating, I see the body of that review and the name of the reviewer that submitted that review.
-
 require 'rails_helper'
 
 describe "User sees a book page" do
 
-  # let (:book) { Book.create!(id: 1, title: "Throne of Glass") }
-  # let (:user1) { User.create!(id: 1, name: "User 1") }
-  # let (:user2) { User.create!(id: 2, name: "User 2") }
-  # let (:user3) { User.create!(id: 3, name: "User 3") }
-
   before do
     book = Book.create!(id: 1, title: "Throne of Glass")
-    user1 = User.create!(id: 1, name: "User 1")
-    user2 = User.create!(id: 2, name: "User 2")
-    user3 = User.create!(id: 3, name: "User 3")
+    User.create!(id: 1, name: "User 1")
+    User.create!(id: 2, name: "User 2")
+    User.create!(id: 3, name: "User 3")
     Review.create!(rating: 1, content: "this book sucks", user_id: 1, book_id: 1)
     Review.create!(rating: 3, content: "meh", user_id: 2, book_id: 1)
     Review.create!(rating: 5, content: "this book is AMAZING", user_id: 3, book_id: 1)
@@ -35,27 +18,26 @@ describe "User sees a book page" do
   end
 
   scenario "the book has reviews" do
-    save_and_open_page
     expect(page).to have_content("Reviews")
     expect(page).to have_content("this book sucks")
     expect(page).to have_content("meh")
     expect(page).to have_content("this book is AMAZING")
-
   end
 
   scenario "the book has an average rating" do
-
+    save_and_open_page
+    expect(page).to have_content("Average Rating: 3")
   end
 
   scenario "the book has a highest rating and review" do
-
+    expect(page).to have_content("Lowest Rating: 1")
+    expect(page).to have_content("Reviewer: User 1")
+    expect(page).to have_content("this book sucks")
   end
 
   scenario "the book has a lowest rating and review" do
-
-  end
-
-  scenario "the book has a title" do
-
+    expect(page).to have_content("Highest Rating: 5")
+    expect(page).to have_content("Reviewer: User 3")
+    expect(page).to have_content("this book is AMAZING")
   end
 end

5706e6b443b71ae2501a2a11ee3992b5e94408e6
Josi <junipertmcclellan@gmail.com>
2017-10-27 11:01:42 -0600

    spec user seeing ratings

--- spec/features/book_show_page_spec.rb
+++ spec/features/book_show_page_spec.rb
@@ -1,23 +1,36 @@
 feature "When someone visits a book's show page" do
 
   background do
-    book = Book.create!(title: "Grimus")
-    visit book_path(book)
+    book = create(:book, title: "Grimus", id: 1)
   end
 
   scenario "they see that book's title" do
+    visit book_path(1)
     expect(page).to have_content("Grimus")
+  end
+
+  describe "they see ratings" do
+    background do
+      1.upto(4){ |n| create(:review, book_id: 1, rating: n) }
+    end
+
+    scenario "including a header that includes the highest rating for that book" do
+      expect(page).to have_content("Highest Rating: 4")
+    end
+    scenario "including a header that includes the lowest rating for that book" do
+      expect(page).to have_content("Lowest Rating: 1")
+    end
   end
 
   describe "they see a list of reviews for that book" do
     background do
       Reveiew.create!(
-        book: Book.last,
+        book_id: 1,
         body: "Best book about birds!",
         rating: 4,
         user: User.create(name: 'Joseph Anton')
       )
-      vist book_path(Book.last)
+      vist book_path(1)
     end
     scenario "including the body" do
       expect(page).to have_content("Best book about birds!")

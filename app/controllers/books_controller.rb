4c8391b52d44976c0ce882a64d725ebd512fd3f5
abarnes26 <abarnes26@yahoo.com>
2017-12-22 09:50:17 -0700

    high rating test passing

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
     @avg_rating = @book.reviews.average(:rating).to_i
+    @high_rating = @book.reviews.maximum(:rating).to_i
   end
 
 end
--- spec/books/user_visits_a_page_for_a_book_spec.rb
+++ spec/books/user_visits_a_page_for_a_book_spec.rb
@@ -21,8 +21,31 @@ describe "a user visits a book show page" do
                           user: user2)
 
       visit "/books/#{book.id}"
-      
+
       expect(page).to have_content("Average Rating: 4")
+    end
+
+    it "the user sees the highest rating for that book" do
+      book = Book.create(title: "Old Man and the Sea")
+      user = User.create(name: "John")
+      user1 = User.create(name: "Hank")
+      user2 = User.create(name: "Holly")
+      review = Review.create(rating: 5,
+                          body: "A Charming Tale",
+                          book: book,
+                          user: user)
+      review = Review.create(rating: 3,
+                          body: "A Charmingish Tale",
+                          book: book,
+                          user: user1)
+      review = Review.create(rating: 4,
+                          body: "What is this book about again?",
+                          book: book,
+                          user: user2)
+
+      visit "/books/#{book.id}"
+
+      expect(page).to have_content("Highest Rating: 5")
     end
   end
 end

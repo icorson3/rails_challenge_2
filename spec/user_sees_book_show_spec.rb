a471a91643f9d5f946e629fdf099f46700d08c32
TMad <madsen.tyler@gmail.com>
2017-12-22 09:55:01 -0700

    add to book show and feature test

--- spec/user_sees_book_show_spec.rb
+++ spec/user_sees_book_show_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 describe "a user visits the book show page" do
-  context "a user visits the station show" do
-    context "the user sees book title, reviews, & rating" do
-      context "the user sees the user that created that review" do
         it "user visits the book show page" do
           book = Book.new(title: "LOTR")
           user1 = User.new(name: "Tyler")
@@ -11,7 +8,7 @@ describe "a user visits the book show page" do
           review1 = Review.new(book_id: book.id, user_id: user1.id, body: "Awesome Book", rating: 4)
           review2 = Review.new(book_id: book.id, user_id: user2.id, body: "Was Ok", rating: 2)
 
-          visit book_path(book)
+          visit "/books/#{book.id}"
 
           expect(current_path).to eq("/books/#{book.id}")
 
@@ -22,8 +19,5 @@ describe "a user visits the book show page" do
           expect(page).to have_content("3")
           expect(page).to have_content("#{review2.rating}")
           expect(page).to have_content("#{review2.body}")
-        end
-      end
-    end
   end
 end

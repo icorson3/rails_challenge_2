1f1b86836253b0b64136a2167e1420831e3dba3b
seoulstice <jungyoungjoon@gmail.com>
2017-12-22 09:23:06 -0700

    made reviews migration

--- spec/features/books/users_visits_book_show_page_spec.rb
+++ spec/features/books/users_visits_book_show_page_spec.rb
@@ -1,1 +1,14 @@
 require "rails_helper"
+
+describe "When the User navigates to the book show page" do
+  it "the User can see the show page" do
+    book = Book.create(title: "The BFG")
+
+    visit book_path(book)
+
+    expect(page).to have_content("The BFG")
+    
+  end
+
+
+end

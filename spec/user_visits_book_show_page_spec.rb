1082dea8ff565abd7aef7793d20dae5dd24583ba
nico24687 <theonlynico@hotmail.com>
2017-12-22 09:23:20 -0700

    set up tests and relationships

---
+++ spec/user_visits_book_show_page_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe "a user visits the show page for a book" do
+  it "they see the title of the book" do
+    user = User.create(name: "Nico")
+    book = Book.create(title: "Harry Potter")
+    review = Review.create(book_id: book.id, rating: 5, body: "Good if you like spells", user_id: user.id)
+
+    visit book_path(book)
+
+    expect(page).to have_content("Harry Potter")
+  end
+  it "sees the review of the book and the rating" do
+    user = User.create(name: "Nico")
+    book = Book.create(title: "Harry Potter")
+    review = Review.create(book_id: book.id, rating: 5, body: "Good if you like spells", user_id: user.id)
+   
+    visit book_path(book)
+
+    expect(page).to have_content("Good if you like spells")
+    expect(page).to have_content(4)
+    expect(page).to have_content("Nico")
+  end
+end
+
+describe "when the user visit the page" do
+  it "sees the average rating of the book" do
+    user = User.create(name: "Nico")
+    book = Book.create(title: "Harry Potter")
+    review = Review.create(book_id: book.id, rating: 5, body: "Good if you like spells", user_id: user.id)
+    review = Review.create(book_id: book.id, rating: 3, body: "Would have prefered more potions", user_id: user.id)
+    
+    visit book_path(book)
+
+    expect(page).to have_content(4)
+    expect(page).to have_content(5)
+    expect(page).to have_content(3)
+  end
+end

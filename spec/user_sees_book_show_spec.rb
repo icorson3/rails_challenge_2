cfa47656d3fde4824eca97bbff010015708462fb
TMad <madsen.tyler@gmail.com>
2017-12-22 09:26:59 -0700

    update test to include 2nd review and rating

--- spec/user_sees_book_show_spec.rb
+++ spec/user_sees_book_show_spec.rb
@@ -6,17 +6,22 @@ describe "a user visits the book show page" do
       context "the user sees the user that created that review" do
         it "user visits the book show page" do
           book = Book.new(title: "LOTR")
-          user = User.new(username: "Tyler")
-          review = Review.new(book_id: book.id, user_id: user.id, body: "Awesome Book")
+          user1 = User.new(username: "Tyler")
+          user2 = User.new(username: "James")
+          review1 = Review.new(book_id: book.id, user_id: user1.id, body: "Awesome Book", rating: 4)
+          review2 = Review.new(book_id: book.id, user_id: user2.id, body: "Was Ok", rating: 2)
 
           visit book_path(book)
 
           expect(current_path).to eq("/books/#{book.id}")
 
           expect(page).to have_content("#{book.title}")
-          expect(page).to have_content("#{user.username}")
-          expect(page).to have_content("#{review.body}")
-
+          expect(page).to have_content("#{user1.username}")
+          expect(page).to have_content("#{review1.body}")
+          expect(page).to have_content("#{review1.rating}")
+          expect(page).to have_content("3")
+          expect(page).to have_content("#{review2.rating}")
+          expect(page).to have_content("#{review2.body}")
         end
       end
     end

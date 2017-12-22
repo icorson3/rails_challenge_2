5fcd7b4fa5786b58e53f2d8f144e6f8415fce67f
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-22 14:38:31 -0700

    need review to show up

--- spec/features/user_visits_book_show_page_spec.rb
+++ spec/features/user_visits_book_show_page_spec.rb
@@ -4,12 +4,15 @@ describe 'a user visits the book show page' do
    before(:each) do 
     @book = create(:book)
     @user = create(:user)
-    @book.reviews.create!(body: 'a review', book_id: @book.id, user_id: @user.id)
+    @book.reviews.create!(body: 'a review', rating: 1, book_id: @book.id, user_id: @user.id)
    end
   it 'sees the book title and a list of reviews with the user that created a review' do
     visit "/books/#{@book.id}"
 
+    save_and_open_page
+
     expect(page).to have_content 'Game of Thrones'
+    expect(page).to have_content '1'
     expect(page).to have_content 'a review'
     expect(page).to have_content "#{@user.name}"
   end

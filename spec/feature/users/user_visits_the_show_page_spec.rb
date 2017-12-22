7274eb333856f95dac331f11b8678274acd75451
Ecksi <jimmy.dechant@gmail.com>
2017-12-22 09:30:55 -0700

    add user visits show page feature test outline

--- spec/feature/users/user_visits_the_show_page_spec.rb
+++ spec/feature/users/user_visits_the_show_page_spec.rb
@@ -1,1 +1,42 @@
 require 'rails_helper'
+
+desribe "When a user visits the show page" do
+  before(:each) do
+    visit book_path
+  end
+
+  scenario "the user sees the header" do
+    it "sees the highest rating for that book with its review" do
+      expect(page).to have_content("Highest Rating: #{@book.rating.max}")
+      # make a review here
+      expect(page).to have_content("Comment by:")
+      expect(page).to have_content("body")
+    end
+
+    it "sees the lowest rating for that book with its review" do
+      expect(page).to have_content("Lowest Rating: #{@book.rating.min}")
+      # make a review here
+      expect(page).to have_content("Comment by:")
+      expect(page).to have_content("body")
+    end
+  end
+
+  scenario "the user sees the body" do
+    it "sees the book title" do
+      expect(page).to have_content("#{@book.title}")
+    end
+
+    it "sees the average rating for that book" do
+      expect(page).to
+      # make methods for min max avg ratings
+      have_content("#{@book.rating.sum / @book.rating.count}")
+    end
+
+    it "sees a list of reviews" do
+      # make reviews here
+      expect(page).to have_content("Comment by: ")
+      expect(page).to have_content("Comment by: ")
+      expect(page).to have_content("Comment by: ")
+    end
+  end
+end

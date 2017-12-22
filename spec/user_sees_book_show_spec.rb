e6753a105a878b69e2544b65f8d9f9fc247aa1bd
TMad <madsen.tyler@gmail.com>
2017-12-22 09:36:16 -0700

    update username to name in test and add end to book show view

--- spec/user_sees_book_show_spec.rb
+++ spec/user_sees_book_show_spec.rb
@@ -6,8 +6,8 @@ describe "a user visits the book show page" do
       context "the user sees the user that created that review" do
         it "user visits the book show page" do
           book = Book.new(title: "LOTR")
-          user1 = User.new(username: "Tyler")
-          user2 = User.new(username: "James")
+          user1 = User.new(name: "Tyler")
+          user2 = User.new(name: "James")
           review1 = Review.new(book_id: book.id, user_id: user1.id, body: "Awesome Book", rating: 4)
           review2 = Review.new(book_id: book.id, user_id: user2.id, body: "Was Ok", rating: 2)
 
@@ -16,7 +16,7 @@ describe "a user visits the book show page" do
           expect(current_path).to eq("/books/#{book.id}")
 
           expect(page).to have_content("#{book.title}")
-          expect(page).to have_content("#{user1.username}")
+          expect(page).to have_content("#{user1.name}")
           expect(page).to have_content("#{review1.body}")
           expect(page).to have_content("#{review1.rating}")
           expect(page).to have_content("3")

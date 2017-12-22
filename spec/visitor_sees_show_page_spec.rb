56488b3d2eb3f8d6dbfea4d35b3ac0eba801e269
Tyjoo26 <Tyjoo21@gmail.com>
2017-12-22 09:39:53 -0700

    Build out first feature test, created review model, created relationships betweenreviews user and books

---
+++ spec/visitor_sees_show_page_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "As a visitor" do
+  describe "when i go to book show page" do
+    before :each do
+      @user = User.create!(name: "Tim")
+      @user_2 = User.create!(name: "James")
+      @user_3 = User.create!(name: "Stella")
+
+      @book = Book.create!(title: "Barnia")
+
+      @review_1 = Review.create!(review_title: "jesus", body: "Terrible", user: @user, rating: 1)
+      @review_2 = Review.create!(review_title: "Sooo Hot right now", body: "Great", user: @user_2, rating: 5)
+      @review_3 = Review.create!(review_title: "Lukewarm", body: "ok", user: @user_3, rating: 3)
+
+      visit book_path(@book)
+    end
+    it "displays book title, list of reviews for that book, the body of the review, the user who created taht review, and it also displays rating" do
+
+      expect(page).to have_content("jesus")
+      expect(page).to have_content("Sooo Hot right now")
+      expect(page).to have_content("Lukewarm")
+
+      expect(page).to have_content("Terrible")
+      expect(page).to have_content("Sooo Hot right now")
+      expect(page).to have_content("ok")
+
+      expect(page).to have_content("Tim")
+      expect(page).to have_content("James")
+      expect(page).to have_content("Stella")
+
+      expect(page).to have_content(1)
+      expect(page).to have_content(5)
+      expect(page).to have_content(3)
+
+    end
+  end
+end

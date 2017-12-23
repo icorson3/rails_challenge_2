2ce06e3e65faf7bd07400865328bfaab82d82f4b
ZacharyLandes <zach.g.landes@gmail.com>
2017-12-23 12:10:25 -0700

    add book spec

---
+++ spec/features/user_can_see_book_review_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'book reviews' do
+  context 'When a user visit a book show page' do
+    it  'see the book title and a list of reviews for that book' do
+
+    end
+  end
+end
+
+
+
+
+
+
+# , including the body and the user that created that review. I also see the rating (1-5; you do not need to validate that this falls within this range) that the user gave the book.

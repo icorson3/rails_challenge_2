0e87742a3ad8b195dbb43d509b2e006847adf374
ZacharyLandes <zach.g.landes@gmail.com>
2017-12-23 12:34:59 -0700

    pass first test

--- spec/features/user_can_see_book_review_spec.rb
+++ spec/features/user_can_see_book_review_spec.rb
@@ -3,8 +3,33 @@ require 'rails_helper'
 describe 'book reviews' do
   context 'When a user visit a book show page' do
     it  'see the book title and a list of reviews for that book' do
+      book = Book.create(title: 'Left hand of darkness')
+      user = User.create(name: "Joe")
+      review = book.reviews.create(body: "it was good", rating: 4, user_id: user.id, book_id: book.id)
+
+      visit "/books/#{book.id}"
+
+      expect(page).to have_content("Left hand of darkness")
+      expect(page).to have_content("it was good")
+      expect(page).to have_content(4)
+
 
     end
+
+    #
+    # it  'see the user that created the review' do
+    #   book = Book.create(title: 'Left hand of darkness')
+    #   user = User.create(name: "Joe")
+    #   review = book.reviews.create(body: "it was good", rating: 4, user_id: user.id, book_id: book.id)
+    #
+    #   visit "/books/#{book.id}"
+    #
+    #   expect(page).to have_content("Left hand of darkness")
+    #   expect(page).to have_content("it was good")
+    #   expect(page).to have_content(4)
+    #
+    #
+    # end
   end
 end
 

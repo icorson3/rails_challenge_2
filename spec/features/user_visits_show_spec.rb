ceb64a0478a6ef01e9ebef4da6791e2af905b989
Tim Tyrrell <timomitchel23@gmail.com>
2017-12-27 16:08:25 -0700

    first test passing

--- spec/features/user_visits_show_spec.rb
+++ spec/features/user_visits_show_spec.rb
@@ -8,8 +8,10 @@ describe 'a user visits the show page' do
     visit "/books/#{@book.id}"
   end
   it 'sees the book title and a list of reviews' do
+    save_and_open_page
     expect(page).to have_content(@book.title)
     expect(page).to have_content('a review')
     expect(page).to have_content '1'
+    expect(page).to have_content 'timo'
   end
 end

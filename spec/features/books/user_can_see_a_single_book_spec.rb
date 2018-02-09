c1977b0eee51d728cf1d8a540b8d182253b7cc94
Luke Chambers <lukenatchambers@gmail.com>
2018-02-09 10:49:59 -0700

    First user story complete

--- spec/features/books/user_can_see_a_single_book_spec.rb
+++ spec/features/books/user_can_see_a_single_book_spec.rb
@@ -4,12 +4,22 @@ describe "As a User" do
   before :each do
     @user = User.create!(name: "Eupanifahorious")
     @book = Book.create!(title: "Opakawagalaga")
+    @review =Review.create(title: "Box of Secrets", body: "Da wey", book: @book, user: @user, rating: 5)
   end
   describe "when I visit the books/:id page" do
     it "I can see the book title" do
       visit book_path(@book)
 
       expect(page).to have_content(@book.title)
+    end
+
+    it "I can see the reviews associated with that book" do
+      visit book_path(@book)
+
+      expect(page).to have_content(@review.title)
+      expect(page).to have_content(@review.body)
+      expect(page).to have_content(@review.user.name)
+      expect(page).to have_content(@review.rating)
     end
 
   end

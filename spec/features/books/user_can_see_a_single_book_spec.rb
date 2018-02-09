ea219b0e5880a7be4cfb3f1f8ca6409daa263f67
Luke Chambers <lukenatchambers@gmail.com>
2018-02-09 11:01:53 -0700

    Test passing for average rating

--- spec/features/books/user_can_see_a_single_book_spec.rb
+++ spec/features/books/user_can_see_a_single_book_spec.rb
@@ -4,7 +4,7 @@ describe "As a User" do
   before :each do
     @user = User.create!(name: "Eupanifahorious")
     @book = Book.create!(title: "Opakawagalaga")
-    @review =Review.create(title: "Box of Secrets", body: "Da wey", book: @book, user: @user, rating: 5)
+    @review = Review.create(title: "Box of Secrets", body: "Da wey", book: @book, user: @user, rating: 5)
   end
   describe "when I visit the books/:id page" do
     it "I can see the book title" do
@@ -13,13 +13,21 @@ describe "As a User" do
       expect(page).to have_content(@book.title)
     end
 
-    it "I can see the reviews associated with that book" do
+    it "I can see the reviews associated with the book" do
       visit book_path(@book)
 
       expect(page).to have_content(@review.title)
       expect(page).to have_content(@review.body)
       expect(page).to have_content(@review.user.name)
       expect(page).to have_content(@review.rating)
+    end
+
+    it "I can see the average rating for the book" do
+      Review.create(title: "Box of Stupids", body: "Da bad wey", book: @book, user: @user, rating: 3)
+
+      visit book_path(@book)
+
+      expect(page).to have_content("Average Rating: 4")
     end
 
   end

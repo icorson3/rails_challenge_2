f5773e9cad6c0a03f08f5f262cc13aa9a4e9e1bc
Luis Garcia <garcia@yahoo.com>
2018-02-09 10:17:10 -0700

    initial commit

---
+++ spec/features/user_visits_the_book_show_page_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "User visits the book show page" do
+  describe "when a visitor visits the user show page" do
+    it "the see the book title and reviews" do
+      book = Book.create!(title: "Avatar")
+      visit book_path
+
+      expect(page).to have_content("Avatar")
+    
+    end
+  end
+end

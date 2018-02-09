0803c1d88f395ed059448436cae2def59149c467
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-09 10:53:24 -0700

    highest rating added

--- spec/features/user_can_see_avg_rating_on_book_show_spec.rb
+++ spec/features/user_can_see_avg_rating_on_book_show_spec.rb
@@ -13,5 +13,16 @@ describe "As a user" do
 
       expect(page).to have_content("Average Rating: 4")
     end
+
+    it "I can see the highest rating for the book" do
+    book = Book.create!(title: "Infinite Jest")
+    rating = Rating.create!(rating: 4, book_id: book.id)
+    rating = Rating.create!(rating: 5, book_id: book.id)
+    user = User.create!(name: "Jim")
+
+    visit book_path(book)
+
+    expect(page).to have_content("Highest Rating: 5")
+  end
   end
 end

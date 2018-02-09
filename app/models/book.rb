5548e683b9fba92c41fe466c8ceec6623e240613
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-09 10:53:21 -0700

    Adds lowest and highest rating to book show

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,15 @@ class Book < ApplicationRecord
     reviews.average(:rating)
   end
 
-  def highest_rating
-    reviews.order(:rating).last.rating
+  def sort_reviews_by_rating
+    reviews.order(:rating)
   end
+
+  def highest_rating
+    sort_reviews_by_rating.last.rating
+  end
+
+  def lowest_rating
+    sort_reviews_by_rating.first.rating
+  end 
 end

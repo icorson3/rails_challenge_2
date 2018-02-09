e97c922a405425aed70ae19b41ea8928c2c961ee
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-09 10:53:26 -0700

    Modify feature book test to include to multiple reviews. Edit book show action. Create book show view template.

--- spec/features/books/user_views_one_book_spec.rb
+++ spec/features/books/user_views_one_book_spec.rb
@@ -4,15 +4,20 @@ describe "user can see one book and list of reviews for that book, including bod
   context "user links to book show page" do
     scenario "user sees book info, reviews, users and ratings" do
       book = Book.create(title: "Little Women")
-      user = User.create(name: "Ellen")
-      review = Review.create(body: "This book is a classic.", rating: 4, book_id: book.id, user: user.id)
+      user_1 = User.create(name: "Ellen")
+      user_2 = User.create(name: "Trevor")
+      review_1 = Review.create(body: "This book is a classic.", rating: 4, book_id: book.id, user_id: user_1.id)
+      review_2 = Review.create(body: "This book is a bore.", rating: 1, book_id: book.id, user_id: user_2.id)
 
       visit book_path(book)
 
       expect(page).to have_content(book.title)
-      expect(page).to have_content(user.name)
-      expect(page).to have_content(review.body)
-      expect(page).to have_content(review.rating)
+      expect(page).to have_content(user_1.name)
+      expect(page).to have_content(user_2.name)
+      expect(page).to have_content(review_1.body)
+      expect(page).to have_content(review_1.rating)
+      expect(page).to have_content(review_2.body)
+      expect(page).to have_content(review_2.rating)
     end
   end
-end 
+end

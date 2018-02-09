5320c1e8ce2ef1c9a9b6b9dc93870d8c0317ef9d
Ellen Cornelius <ecorneli@iwu.edu>
2018-02-09 10:57:56 -0700

    Add book model test for average rating instance method.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,6 @@
 class Book < ApplicationRecord
   has_many :reviews
+
+  def average_rating
+  end 
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -7,4 +7,18 @@ describe Book do
       expect(book).to respond_to(:reviews)
     end
   end
+
+  describe "instance methods" do
+    describe "#average_rating" do
+      it "returns average rating for book" do
+        book = Book.create(title: "Little Women")
+        user_1 = User.create(name: "Ellen")
+        user_2 = User.create(name: "Trevor")
+        review_1 = Review.create(body: "This book is a classic.", rating: 4, book_id: book.id, user_id: user_1.id)
+        review_2 = Review.create(body: "This book is a bore.", rating: 1, book_id: book.id, user_id: user_2.id)
+
+        expect(book.average_rating).to eq(2.5)
+      end
+    end
+  end
 end

cdb761eadd2cf5e0f1e7de6b2ab4998b6ad89a43
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-09 10:50:45 -0700

    average done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,6 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    # @ratings = @book.
   end
 
 end
--- spec/features/user_can_see_avg_rating_on_book_show_spec.rb
+++ spec/features/user_can_see_avg_rating_on_book_show_spec.rb
@@ -3,14 +3,15 @@ require 'rails_helper'
 describe "As a user" do
   context "when I go to the books show page" do
     it "I can see the books average rating" do
-      rating = Rating.create!(rating: 5)
-      rating = Rating.create!(rating: 4)
-      book = Book.create!(title: "Infinite Jest", rating_id: rating.id)
+      book = Book.create!(title: "Infinite Jest")
+      rating = Rating.create!(rating: 4, book_id: book.id)
+      rating = Rating.create!(rating: 5, book_id: book.id)
+
       user = User.create!(name: "Jim")
 
       visit book_path(book)
 
-      expect(page).to have_content("Average Rating: 4.5")
+      expect(page).to have_content("Average Rating: 4")
     end
   end
 end

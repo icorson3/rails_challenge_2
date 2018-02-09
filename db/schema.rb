b753a54528b182634949fb9ca3cbe41863e33d7f
Andrew Dymond <andy.dymond@gmail.com>
2018-02-09 10:32:27 -0700

    Add spec to test ratings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209172521) do
+ActiveRecord::Schema.define(version: 20180209173102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20180209172521) do
     t.bigint "book_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "rating"
     t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
--- spec/features/user_sees_one_book_spec.rb
+++ spec/features/user_sees_one_book_spec.rb
@@ -1,14 +1,16 @@
 require "rails_helper"
 
 describe "User sees one book" do
-  it "displays book title and list of reviews" do
+  it "displays book title, list of reviews and ratings" do
     book = Book.create(title: "title")
-    review_1 = book.reviews.create(reviewer: "reviewer 1", body: "review 1")
-    review_2 = book.reviews.create(reviewer: "reviewer 2", body: "review 2")
+    review_1 = book.reviews.create(reviewer: "reviewer 1", body: "review 1", rating: 10)
+    review_2 = book.reviews.create(reviewer: "reviewer 2", body: "review 2", rating: 1)
     visit "/books/#{book.id}"
 
     expect(page).to have_content(book.title)
     expect(page).to have_content(review_1.reviewer)
     expect(page).to have_content(review_2.reviewer)
+    expect(page).to have_content(review_2.rating)
+    expect(page).to have_content(review_2.rating)
   end
 end

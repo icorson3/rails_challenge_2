4f7aabe09733531e75ebd1c728604212285883f5
mgmilton <matt.milton@gmail.com>
2018-02-09 10:12:42 -0700

    adds migration for reviews

---
+++ db/migrate/20180209170945_create_reviews.rb
@@ -0,0 +1,9 @@
+class CreateReviews < ActiveRecord::Migration[5.1]
+  def change
+    create_table :reviews do |t|
+      t.text :body
+      t.references :user, foreign_key: true
+      t.references :book, foreign_key: true
+    end
+  end
+end

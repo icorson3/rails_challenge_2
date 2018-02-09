00dac4bf5495d62f3dde58ba1b3d7c7ecc203faa
kylesallette <kylesallette@gmail.com>
2018-02-09 10:16:01 -0700

    adding seeds

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,9 @@
+class BooksController < ApplicationController
+
+
+
+
+
+
+
+end 
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,12 @@
 class Book < ApplicationRecord
-  
+
+  def index
+    @books = Book.all
+  end 
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+
 end

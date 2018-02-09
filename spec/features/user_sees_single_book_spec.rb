1d0703e3a476aba05d74f8fe4849754f372104f9
Joe Jobes <jmrjobes@gmail.com>
2018-02-09 10:56:21 -0700

    Add to feature test to show average and top rating

--- spec/features/user_sees_single_book_spec.rb
+++ spec/features/user_sees_single_book_spec.rb
@@ -5,12 +5,16 @@ describe "user can see single book" do
     it "sees single book" do
       book = create(:book)
       user = create(:user)
-      review = create(:review, user: user, book: book)
+      review = create(:review, rating: 4, user: user, book: book)
+      review2 = create(:review, rating: 5, user: user, book: book)
+      review3 = create(:review, rating: 3, user: user, book: book)
       visit book_path(book)
 
       expect(page).to have_content(book.title)
       expect(page).to have_content(user.name)
       expect(page).to have_content(review.rating)
+      expect(page).to have_content(book.average_rating)
+      expect(page).to have_content(book.top_rating)
     end
   end
 end

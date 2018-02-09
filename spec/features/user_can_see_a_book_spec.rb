6588b8e6feec2b531e180c65739da0b7d2d40624
amj133 <amj@vt.edu>
2018-02-09 10:12:38 -0700

    project setup - book model test written

---
+++ spec/features/user_can_see_a_book_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+# When I visit a book show page, I see the book title and a list of reviews for that book,
+# including the body and the user that created that review.
+# I also see the rating (1-5; you do not need to validate that this falls within this range)
+# that the user gave the book.
+
+describe "user can see individual book information" do
+  context "visits book show page" do
+    it "displays book informations" do
+
+    end
+  end
+end

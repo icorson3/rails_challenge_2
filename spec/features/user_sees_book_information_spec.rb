69cc8ceb7434aeb05f74435d6345e361e902adbe
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-02-09 10:16:19 -0700

    Adds feature test for book info show

--- spec/features/user_sees_book_information_spec.rb
+++ spec/features/user_sees_book_information_spec.rb
@@ -1,1 +1,23 @@
 require 'rails_helper'
+
+describe "user sees book information" do
+  describe "user visits book_path" do
+    it "they can see the book details" do
+      book = Book.create!(title: "Harry Potter")
+      user_1 = User.create!(name: "Severus Snape")
+      user_2 = User.create!(name: "Ron Weasley")
+      review_1 = (user: user_1, body: "Excellent character development.", rating: 4)
+      review_2 = (user: user_2, body: "Superb romance.", rating: 3)
+
+      visit book_path(book)
+
+      expect(page).to have_content("Harry Potter")
+      expect(page).to have_content("Severus Snape")
+      expect(page).to have_content("Excellent character development.")
+      expect(page).to have_content(4)
+      expect(page).to have_content("Ron Weasley")
+      expect(page).to have_content("Superb romance.")
+      expect(page).to have_content(3)
+    end
+  end
+end

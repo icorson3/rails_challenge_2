44e464e7bdd4d84e8c4e026e6122cb8c84089730
Luis Garcia <garcia@yahoo.com>
2018-02-09 10:21:17 -0700

    Create Book controller

--- spec/features/user_visits_the_book_show_page_spec.rb
+++ spec/features/user_visits_the_book_show_page_spec.rb
@@ -4,7 +4,7 @@ describe "User visits the book show page" do
   describe "when a visitor visits the user show page" do
     it "the see the book title and reviews" do
       book = Book.create!(title: "Avatar")
-      visit book_path
+      visit book_path(book)
 
       expect(page).to have_content("Avatar")
     

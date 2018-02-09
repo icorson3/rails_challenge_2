f3fd090072ab2e09073aa72c054620a0d69f763c
ruby_road_to_ruin <zac.palmquist@gmail.com>
2018-02-09 10:54:23 -0700

    adds lowest rating show

--- spec/features/user_can_see_avg_rating_on_book_show_spec.rb
+++ spec/features/user_can_see_avg_rating_on_book_show_spec.rb
@@ -24,5 +24,16 @@ describe "As a user" do
 
     expect(page).to have_content("Highest Rating: 5")
   end
+
+    it "I can see the lowest rating for the book" do
+    book = Book.create!(title: "Infinite Jest")
+    rating = Rating.create!(rating: 4, book_id: book.id)
+    rating = Rating.create!(rating: 5, book_id: book.id)
+    user = User.create!(name: "Jim")
+
+    visit book_path(book)
+
+    expect(page).to have_content("Lowest Rating: 4")
+  end
   end
 end

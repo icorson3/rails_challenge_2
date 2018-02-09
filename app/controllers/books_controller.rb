11cc245ed5fe2bd874333389ec5bb5e260748da8
Luis Garcia <garcia@yahoo.com>
2018-02-09 11:02:41 -0700

    final commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,7 @@
 class BooksController < ApplicationController
 
   def show
-    @user = User.find(params[user_id])
+    @user = User.find(params[:id])
     @book = Book.find(params[:id])
   end
 
--- spec/features/user_visits_the_book_show_page_spec.rb
+++ spec/features/user_visits_the_book_show_page_spec.rb
@@ -2,13 +2,12 @@ require 'rails_helper'
 
 describe "User visits the book show page" do
   describe "when a visitor visits the user show page" do
-    it "the see the book title and reviews" do
+    scenario "the see the book title and reviews" do
       User.create!(name: "Mike")
-      require 'pry'; binding.pry
       book = Book.create!(title: "Avatar")
-      save_and_open_page
       visit book_path(book)
 
+      save_and_open_page
       expect(page).to have_content("Avatar")
 
     

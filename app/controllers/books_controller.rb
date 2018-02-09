8eab7ed51c305515a793405ba5289e40fba31050
mgmilton <matt.milton@gmail.com>
2018-02-09 10:16:46 -0700

    updates reviews

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,12 @@
 class BooksController < ApplicationController
+
+  def show
+
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180209170945) do
+ActiveRecord::Schema.define(version: 20180209171628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20180209170945) do
     t.text "body"
     t.bigint "user_id"
     t.bigint "book_id"
+    t.integer "rating"
     t.index ["book_id"], name: "index_reviews_on_book_id"
     t.index ["user_id"], name: "index_reviews_on_user_id"
   end

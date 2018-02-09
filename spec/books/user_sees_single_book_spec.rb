28982f0cb96dbbcd56ea3656b3dfebac7b757d85
Joe Jobes <jmrjobes@gmail.com>
2018-02-09 10:21:34 -0700

    Finish show feature test setup

--- spec/books/user_sees_single_book_spec.rb
+++ spec/books/user_sees_single_book_spec.rb
@@ -5,8 +5,12 @@ describe "user can see single book" do
     it "sees single book" do
       book = create(:book)
       user = create(:user)
+      review = create(:review)
       visit book_path(book)
 
       expect(page).to have_content(book.title)
       expect(page).to have_content(user.name)
-      expect(page).to have_content(user.reviews)
+      expect(page).to have_content(user.reviews.rating)
+    end
+  end
+end

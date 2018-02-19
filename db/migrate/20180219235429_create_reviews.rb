fa6a37a79c5cd33e81678fbbf5668b217d8edd96
kylesallette <kylesallette@gmail.com>
2018-02-19 16:56:40 -0700

    adding foreign keys for review

--- db/migrate/20180219235429_create_reviews.rb
+++ db/migrate/20180219235429_create_reviews.rb
@@ -3,6 +3,8 @@ class CreateReviews < ActiveRecord::Migration[5.1]
     create_table :reviews do |t|
       t.string :body
       t.integer :rating
+      t.references :user, foreign_key: true
+      t.references :book, foreign_key: true 
     end
   end
 end

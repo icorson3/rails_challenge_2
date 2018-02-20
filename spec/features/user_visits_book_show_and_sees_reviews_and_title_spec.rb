60de7eb9f62f0b53ccdb9706279df567b8836106
kylesallette <kylesallette@gmail.com>
2018-02-19 17:13:05 -0700

    adding controller now moving onto view

--- spec/features/user_visits_book_show_and_sees_reviews_and_title_spec.rb
+++ spec/features/user_visits_book_show_and_sees_reviews_and_title_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
     user = User.create!(name: "Kyle")
     review = book.reviews.create!(body: "awesome",rating: 10, user_id: user.id, book_id: book.id)
 
-    visit book_path
+    visit book_path(review)
 
     expect(page).to have_content(book.title)
     expect(page).to have_content(review.body)

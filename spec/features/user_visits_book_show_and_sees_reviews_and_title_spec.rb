3afbb0be5896758be97556200bf3aab63bbd0fb5
kylesallette <kylesallette@gmail.com>
2018-02-19 17:26:45 -0700

    second test passing

--- spec/features/user_visits_book_show_and_sees_reviews_and_title_spec.rb
+++ spec/features/user_visits_book_show_and_sees_reviews_and_title_spec.rb
@@ -16,9 +16,10 @@ end
     it "when they visit book show page"  do
       book = Book.create!(title: "Harry Potter")
       user = User.create!(name: "Kyle")
-      review = book.reviews.create!(body: "awesome",rating: 10, user_id: user.id, book_id: book.id)
-      review = book.reviews.create!(body: "something",rating: 10, user_id: user.id, book_id: book.id)
+      review_1 = book.reviews.create!(body: "awesome",rating: 10, user_id: user.id, book_id: book.id)
+      review_2 = book.reviews.create!(body: "something",rating: 10, user_id: user.id, book_id: book.id)
+      visit book_path(book)
 
       expect(page).to have_content(10)
     end
-  end 
+  end

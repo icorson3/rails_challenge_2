e71a7a865d2b84c122db9ceffb8e7251c2feed69
kylesallette <kylesallette@gmail.com>
2018-02-19 17:28:46 -0700

    third test

--- spec/features/user_visits_book_show_and_sees_reviews_and_title_spec.rb
+++ spec/features/user_visits_book_show_and_sees_reviews_and_title_spec.rb
@@ -23,3 +23,15 @@ end
       expect(page).to have_content(10)
     end
   end
+
+  describe "user sees the highest review of book"  do
+    it "when they visit book show page"  do
+      book = Book.create!(title: "Harry Potter")
+      user = User.create!(name: "Kyle")
+      review_1 = book.reviews.create!(body: "awesome",rating: 20, user_id: user.id, book_id: book.id)
+      review_2 = book.reviews.create!(body: "something",rating: 10, user_id: user.id, book_id: book.id)
+      visit book_path(book)
+
+      expect(page).to have_content("Highest rating 20")
+    end
+  end

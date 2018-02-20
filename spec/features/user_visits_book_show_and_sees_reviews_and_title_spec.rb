54a96a526138eb3d07c4eabff53793e648a867ec
kylesallette <kylesallette@gmail.com>
2018-02-19 17:02:59 -0700

    adding first test

---
+++ spec/features/user_visits_book_show_and_sees_reviews_and_title_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+  describe "user visits book show page"  do
+    it "user sees book title and reviews"  do
+    book = Book.create!(title: "Harry Potter")
+    user = User.create!(name: "Kyle")
+    review = book.reviews.create!(body: "awesome",rating: 10, user_id: user.id, book_id: book.id)
+
+    visit book_path
+
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(book.reviews.body)
+    expect(page).to have_content(book.reviews.rating)
+  end
+end 
